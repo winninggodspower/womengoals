8a9508ef8618e9db07ef16bd3407413c727aff3e
winning godspower <winninggodspower@gmail.com>
2023-06-07 09:17:56 +0100

    added edit and delete blog route

--- blog/blog.py
+++ blog/blog.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, render_template, request, redirect, flash
+from flask import Blueprint, render_template, request, redirect, flash, url_for, current_app
 from .auth import auth
 from .forms import BlogForm
 from flask_ckeditor import CKEditor
 from werkzeug.utils import secure_filename
 import os
 from .models import Post, db
-from flask import current_app
+from flask_wtf.csrf import CSRFProtect
+from flask_wtf.file import FileAllowed
+
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 blog = Blueprint('blog', __name__, url_prefix='/blog')
@@ -13,13 +15,14 @@ blog = Blueprint('blog', __name__, url_prefix='/blog')
 class BlogConfig:
     def __init__(self, app) -> None:
         app.config["UPLOAD_FOLDER"] = os.getenv('UPLOAD_FOLDER')
+        app.config["UPLOAD_SUB_FOLDER"] = os.getenv('UPLOAD_SUB_FOLDER')
         ckeditor = CKEditor(app)
+        csrf = CSRFProtect(app)
 
 
 
 @blog.route("/", methods=['GET', 'POST'])
 def view_all_blog():
-    print(bool(auth.username()))
     all_blogs = Post.query.all()
     return render_template("blog/view_all_blog.html", all_blogs=all_blogs, user_is_authenticated=bool(auth.username()))
 
@@ -46,27 +49,55 @@ def create_blog():
     return render_template("blog/create_blog.html", form=form)
 
 
-@blog.route("/edit_post/<string:blog_title>", methods=["GET", "POST"])
-def edit_blog(blog_title):
-    if request.method == "GET":
-        blog = Post.query.filter_by(title=blog_title).first_or_404()
-        form = BlogForm(title=blog.title, intro=blog.intro, content=blog.content, image=blog.image)
-        return render_template("blog/edit_blog.html", form=form)
+@blog.route("/edit_blog/<string:blog_id>", methods=["GET", "POST"])
+@auth.login_required
+def edit_blog(blog_id):
+    form = BlogForm()
+    # Remove DataRequired validator for image field in the edit route
+    form.image.validators = [FileAllowed(['jpg', 'png'], 'Images only!')]
+    
+    blog = Post.query.filter_by(id=blog_id).first_or_404()
+
+    if form.validate_on_submit():
+        image_path = save_blog_image(form.image.data) # save blog image
+
+        # Update the post with the new data
+        blog.title = form.title.data
+        blog.intro = form.intro.data
+        blog.content = form.content.data
+        blog.image = image_path
+
+        # Save the changes to the database
+        db.session.commit()
+
+        flash('Blog has been updated successfully.', 'success')
+        return redirect(url_for('blog.view_blog', blog_slug=blog.slug))
+
+    form = BlogForm(title=blog.title, intro=blog.intro, content=blog.content, image=blog.image)
+    return render_template("blog/edit_blog.html", form=form, blog=blog)
+    
+
+@blog.route("/delete_blog/<string:blog_id>", methods=["GET"])
+def delete_blog(blog_id):
+    blog = Post.query.get_or_404(blog_id)
+    db.session.delete(blog)
+    db.session.commit()
+    flash("succesfully deleted blog post", "success")
+    return redirect(url_for('blog.view_all_blog'))
     
 
 def save_blog_image(f ):
     filename = secure_filename(f.filename)
-    BLOG_PHOTO_DIR = os.path.join(PROJECT_DIR, current_app.config["UPLOAD_FOLDER"])
+    BLOG_PHOTO_DIR = os.path.join(PROJECT_DIR, current_app.config["UPLOAD_FOLDER"], current_app.config["UPLOAD_SUB_FOLDER"])
 
     # create blog photo directory if it doesnt exist
     if not os.path.exists(BLOG_PHOTO_DIR):
-        print("photo directory exists")
         os.makedirs(BLOG_PHOTO_DIR)
     
     # get full blog_path
     file_path = os.path.join(
-        PROJECT_DIR, current_app.config["UPLOAD_FOLDER"], filename
+        BLOG_PHOTO_DIR, filename
     )
     f.save(file_path)
 
-    return os.path.relpath(file_path, PROJECT_DIR)
+    return f"/{current_app.config['UPLOAD_SUB_FOLDER']}/{filename}"
--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileAllowed, FileRequired
