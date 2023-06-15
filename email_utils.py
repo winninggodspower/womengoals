da18d41254f9e229151e34556b18fc4d59310a6d
winning godspower <winninggodspower@gmail.com>
2023-06-15 18:57:29 +0100

    fixed some typo

--- email_utils.py
+++ email_utils.py
@@ -2,7 +2,7 @@ import subprocess
 from flask import render_template
 
 def send_volunteer_email_php(user_info):
-        msg = render_template("contact_email.html", user = user_info)
+        msg = render_template("volunteer_email.html", user = user_info)
 
         # Command to run the PHP script
         command = ['php', 'php_email_scripts/volunteer_email.php', msg]
